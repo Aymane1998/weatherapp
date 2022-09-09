58e84fc0244649d319eb5f365c2911ecb86cb0b2
Aymane1998 <aymaneelarache@gmail.com>
2022-09-09 18:35:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Search from './components/Search'
+
+function App() {
+ 
+  const handleOnSearchChange=(searchData)=>{
+    console.log(searchData);
+  }
+
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1ddf68ae8dmsh6ea9ed21375deb3p188ba7jsn750d0d574f67',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
